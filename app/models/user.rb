81c06b0ec5476e58bc2b7d5553de197d6cc82655
Dan Frenette <Dan.R.Frenette@gmail.com>
2017-04-22 20:57:52 -0400

    Create models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :chatroom_users
+  has_many :chatrooms, through: :chatroom_users
+  has_many :messages
 end
