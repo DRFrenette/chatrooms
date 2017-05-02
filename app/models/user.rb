1d256c53031f8d18652890d5cf9ee3629ba07235
Dan Frenette <Dan.R.Frenette@gmail.com>
2017-05-01 21:34:42 -0400

    Messages are sent over an open connection in real time

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :chatroom_users
+  has_many :chatroom_users, dependent: :destroy
   has_many :chatrooms, through: :chatroom_users
   has_many :messages
 
