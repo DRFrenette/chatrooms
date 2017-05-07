3a75d22a27ed6923acb7cb5d339f426ae11d3477
Dan Frenette <Dan.R.Frenette@gmail.com>
2017-05-07 16:39:56 -0400

    Style and refactor chatrooms index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def join_or_leave_link(user, chatroom)
+    if user.chatrooms.include?(chatroom)
+      link_to chatroom_chatroom_users_path(chatroom), method: :delete do
+        content_tag(:i, "", class: "icon-line-cross", style: "color: #00CDCD")
+      end
+    else
+      link_to chatroom_chatroom_users_path(chatroom), method: :post do
+        content_tag(:i, "", class: "icon-plus", style: "color: #00CDCD")
+      end
+    end
+  end
 end
