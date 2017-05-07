eae3a21d8743d0523e0ccde0718dab0ab6df08e3
Dan Frenette <Dan.R.Frenette@gmail.com>
2017-05-07 14:05:24 -0400

    Users cannot see the app without signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
   protected
+
+  def after_sign_in_path_for(resource)
+    chatrooms_path
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
+  def new
+    redirect_to new_user_session_path anchor: "tab-register"
+  end
+
   def create
     super
     cookies[:action_cable_id] = resource.id
