5104a7a4e87affbd7101cf38919fe0b8caa8bb8d
Dan Frenette <Dan.R.Frenette@gmail.com>
2017-05-07 14:05:24 -0400

    Users cannot see the app without signing in

--- app/controllers/chatroom_users_controller.rb
+++ app/controllers/chatroom_users_controller.rb
@@ -1,6 +1,4 @@
 class ChatroomUsersController < ApplicationController
-  before_action :authenticate_user!
-
   def create
     @chatroom_user = chatroom.chatroom_users.find_or_create_by(user_id: current_user.id)
     redirect_to chatroom
