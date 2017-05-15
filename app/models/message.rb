270525b71ec9bc03e16c2e58549b7812fcee4d44
Dan Frenette <Dan.R.Frenette@gmail.com>
2017-05-15 17:15:18 -0400

    Public channels are censored

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,15 @@
 class Message < ApplicationRecord
+  require 'obscenity/active_model'
+
   belongs_to :chatroom
   belongs_to :user
 
   validates :body, presence: true
+  validates :body, obscenity: { sanitize: true }, if: :public_message?
+
+  private
+
+  def public_message?
+    !chatroom.private_channel
+  end
 end
